d9935d6bb84cfcccf7924f0567f5e7bdd184d432
Gurden <gurden@darkmatterlabs.org>
2025-05-28 12:24:46 +0200

    update app folder structure and and initialise all sanity files

---
+++ app/studio/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Many to Many Studio',
+  description: 'Collections and all information',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
