c9881137868215efc3e75b6bb240a83cec24761d
Gurden <gurden@darkmatterlabs.org>
2025-08-14 17:36:17 +0200

    text update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,22 @@ export default function Footer() {
   return (
     <footer className="mb-2 bg-gray-200 py-10 shadow-md shadow-orange-800">
       <p className="text-regular mx-10 my-8 text-blue-800">
-        This website takes data information, add here info about Dark Matter
-        Labs, but also credits about use of anything in this website, creative
-        commons but cite the source. Toward the end also add credits about
-        coding, design and concept of the website.
+        This work is part of{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://darkmatterlabs.org/"
+        >
+          Dark Matter Labs
+        </a>
+        ’ Beyond the Rules Lab and under the{' '}
+        <a
+          rel="noopener noreferrer"
+          href="https://creativecommons.org/licenses/by-nc/4.0/"
+        >
+          CC BY-NC
+        </a>{' '}
+        license.
       </p>
     </footer>
   );
