ce16c6049786ef4076cfe0e24bc79a62ba50a41d
Gurden <gurden@darkmatterlabs.org>
2025-10-07 11:57:37 +0200

    rm legal theme from schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,6 @@ import alert from './schemas/alert';
 import deep_code_shift from './schemas/deep_code_shift';
 import insight from './schemas/insight';
 import layer from './schemas/layer';
-import legal_theme from './schemas/legal_theme';
 import story from './schemas/story';
 import system_blocker from './schemas/system_blocker';
 import tool from './schemas/tool';
@@ -15,7 +14,6 @@ export const schema = {
     deep_code_shift,
     insight,
     layer,
-    legal_theme,
     story,
     system_blocker,
     tool,
