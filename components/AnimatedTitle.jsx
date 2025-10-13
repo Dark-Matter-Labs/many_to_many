3bc84ad93f6c5e5bc209e4254870fce8f65faee5
Gurden <gurden@darkmatterlabs.org>
2025-10-13 16:07:32 +0200

    Update AnimatedTitle.jsx

--- components/AnimatedTitle.jsx
+++ components/AnimatedTitle.jsx
@@ -32,7 +32,7 @@ export default function AnimatedTitle({ scrollYProgress }) {
   return (
     <div className={'font-galosText ' + styles.titleContainer}>
       <motion.h1 style={{ opacity: title1Opacity, y: title1Y }}>
-        Brief Introduction (scroll for more)
+        Brief Introduction
       </motion.h1>
 
       <motion.h1 style={{ opacity: title2Opacity, y: title2Y }}>
