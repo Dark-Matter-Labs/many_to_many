4ed2f563870f7964df9af5362ac4f66fc7d17a1b
Gurden <gurden@darkmatterlabs.org>
2025-10-31 12:46:41 +0100

    responsove improvements

--- app/(front-end)/legal-architecture/page.jsx
+++ app/(front-end)/legal-architecture/page.jsx
@@ -103,7 +103,7 @@ export default function LegalToolsPage() {
               </div>
             </div>
 
-            <h2 className="heading-lg mt-8 max-w-md pb-1 text-blue-800">
+            <h2 className="heading-lg mt-8 max-w-md pb-8 text-blue-800">
               Our Learnings
             </h2>
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
